6ed5338667fd60a8d99a658af3e09037dc7b2434
sam <jialin.hou@deeplang.ai>
2024-04-03 15:28:09 +0800

    feat: card component

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -1,5 +1,25 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { PersonStandingIcon } from "lucide-react"
+import Link from "next/link"
+
 export default function Page() {
   return (
-    <div>Login</div>
+    <>
+      <PersonStandingIcon size={80} className="text-pink-500" />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your supportMe account</CardDescription>
+        </CardHeader>
+        <CardContent>Login Form</CardContent>
+        <CardFooter className="justify-between">
+          <small>Did not have an account</small>
+          <Button asChild variant="outline" size="sm">
+            <Link href="/sign-up">Sign Up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
   )
 }
