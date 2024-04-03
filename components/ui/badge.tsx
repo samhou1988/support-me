181bed002ec372f89295bb3c61b2b5f76fec78a1
sam <jialin.hou@deeplang.ai>
2024-04-03 17:58:49 +0800

    feat: badge add success

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -15,6 +15,7 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        success: "bg-green-400 text-black border-transparent",
       },
     },
     defaultVariants: {
