31b45b476fbcda230451828ac8e1c6ef6c74707e
sam <jialin.hou@deeplang.ai>
2024-04-03 16:56:20 +0800

    feat: dashboard

--- components/ui/light-dark-toggle.tsx
+++ components/ui/light-dark-toggle.tsx
@@ -8,7 +8,7 @@ type Props = {
   className?: string;
 }
 
-export default function LightDarkToggle({ className}: Props) {
+export function LightDarkToggle({ className}: Props) {
   const [isDarkMode, setIsDarkMode] = useState(true);
 
   return (
